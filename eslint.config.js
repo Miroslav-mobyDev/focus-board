e1ca2084795c939760c29ef5049d6ff6fc66ee27
Miroslav <pnahuj314@mail.com>
2025-07-15 16:25:15 +0600

    Corrected lint

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+// eslint.config.js
+
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      import: eslintPluginImport,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+  eslintConfigPrettier,
+];
+
+
