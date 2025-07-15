5cfbf1d2cf54ea3c1c811dd2b16c8c0d17eeba24
Miroslav <pnahuj314@mail.com>
2025-07-15 16:59:01 +0600

    Corrected deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
 
-// Путь зависит от твоего GitHub репозитория
-const repoName = 'focus-board';
+
 
 export default defineConfig({
-  base: `/${repoName}/`, // важно: имя репозитория
+  base: '/focus-board/', // важно: имя репозитория
 });
